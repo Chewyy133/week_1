0cbb592be6ba08818642145de347bb323f0477e7
Jesus Morales <jesusmorales141512@gmail.com>
2022-08-09 06:42:46 -0500

    solved sample problem

--- conditions/equality_3_values.js
+++ conditions/equality_3_values.js
@@ -1,2 +1,18 @@
 // three inputs
 // return the number of equal values => 0, 2, 3
+
+const input1 = 40   
+const input2 = 40
+const input3 = 50 
+
+let answer = 0  
+
+if ((input1 === input2) && (input1 ===input3)) {
+    answer = 3
+}
+
+else if ((input1 === input2) || (input2 === input3) || (input1 === input3)) {
+    answer = 2
+}
+
+console.log(answer)
